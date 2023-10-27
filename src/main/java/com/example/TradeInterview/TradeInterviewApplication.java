c8c8e21830d88fab96c9fb517f9e5589ee5e0484
DoanNamTruc <doannamtruc0123@gmail.com>
2023-10-27 14:57:56 +0700

    initial commit

---
+++ src/main/java/com/example/TradeInterview/TradeInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.TradeInterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TradeInterview/TradeInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TradeInterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
